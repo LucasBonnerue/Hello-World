9a252c1d33da0960fcfb1d34c398a67285c0af62
kischtstubbi <31034524+LucasBonnerue@users.noreply.github.com>
2017-08-15 21:53:36 +0200

    Add files via upload

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Lucas
+ */
+public class HelloWorld {
+      public static void main(String[] args) {
+            System.out.println("Hello World");
+            System.out.print("");
+            System.out.print("Ich heisse Lucas Bonnerue bin in der 12. Klasse.");
+            System.out.println("");
+            System.out.print("Ich programmiere gerne :).");
+
+    }
+    
+}
